5e27395581471968b9b5172d0cc60593f29a32a6
timurnav <timurnav@gmail.com>
2015-10-28 10:29:04 +0300

    StAX beginning

--- src/test/java/ru/timurnav/TestStAX.java
+++ src/test/java/ru/timurnav/TestStAX.java
@@ -1,6 +1,7 @@
 package ru.timurnav;
 
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.File;
@@ -12,7 +13,26 @@ public class TestStAX {
     public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
         XMLInputFactory factory = XMLInputFactory.newInstance();
         factory.setProperty(XMLInputFactory.IS_COALESCING, Boolean.TRUE);
-        XMLStreamReader reader = factory.createXMLStreamReader(new FileInputStream(new File("")));
+        XMLStreamReader reader = factory.createXMLStreamReader(new FileInputStream(new File("src\\test\\resources\\shapes1.xml")));
+        try {
+            while (true) {
+
+                switch (reader.getEventType()) {
+                    case XMLStreamConstants.START_ELEMENT:
+
+                        System.out.println("Start element: " + reader.getName());
+                        break;
+                    case XMLStreamConstants.END_ELEMENT:
+                        System.out.println("End element: " + reader.getName());
+                        break;
+                }
+                if (!reader.hasNext())
+                    break;
+                reader.next();
+            }
+        } finally {
+            reader.close();
+        }
     }
 
 }
