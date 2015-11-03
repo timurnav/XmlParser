9ff5236194aca27f5e3c19ddbcb7af0d7381b3d3
timurnav <timurnav@gmail.com>
2015-11-03 17:33:24 +0300

    concurrency refactored

--- src/main/java/ru/timurnav/xmlReader/Printing.java
+++ src/main/java/ru/timurnav/xmlReader/Printing.java
@@ -2,13 +2,16 @@ package ru.timurnav.xmlReader;
 
 import ru.timurnav.model.Shape;
 
+import java.util.concurrent.TimeUnit;
+
 public class Printing implements Runnable {
 
     @Override
     public void run() {
         try {
-            while (Processing.counter.get() != 0 || !Processing.shotdown) {
-                Shape shape = Processing.SHAPE_QUEUE.take();
+            while (!Processing.shotdown || Processing.counter.get() > 0) {
+                Shape shape = Processing.SHAPE_QUEUE.poll(1, TimeUnit.SECONDS);
+                if (shape == null) break;
                 System.out.println(shape);
                 Processing.counter.decrementAndGet();
             }
